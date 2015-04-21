63235008451af21501fd2892e92cde17096e3a17
Alexander Tappin <alextappin@gmail.com>
2015-04-21 22:58:36 +0000

    This is the test

---
+++ r.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#github message. no quotes needed
+commitMessage = ""
+ARGV.each do|a|
+  commitMessage += a
+end
+#puts "This is the message #{commitMessage}"
+system 'git init'
+system 'git add -A'
+system 'git commit -m #{commitMessage}'
+system 'git push origin master'
+system 'alextappin'
+system '101862ajt'
